097aa4100f210f2ad29c0cd04760600dc3c6c9b7
Shritej Mayekar <shritejmayekar@gmail.com>
2020-04-07 17:41:59 +0530

    CHANGED:to staticfilestorage from manifested

--- keepNotes/settings.py
+++ keepNotes/settings.py
@@ -143,4 +143,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
